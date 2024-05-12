c0ee2d2663d17ab246f78377b9ed1faa5ad6e17b
gk2007 <lovezww2011@gmail.com>
2024-05-12 22:15:32 +0800

    Update root.js

--- web/src/router/root.js
+++ web/src/router/root.js
@@ -20,14 +20,14 @@ const constantRouterMap = [
       { path: 'thing', name: 'thing', component: () => import('/@/views/thing.vue') },
       { path: 'comment', name: 'comment', component: () => import('/@/views/comment.vue') },
       { path: 'user', name: 'user', component: () => import('/@/views/user.vue') },
-      { path: 'classification', name: 'classification', component: () => import('/@/views/classification.vue') },
+      { path: 'classification', name: 'classification2', component: () => import('/@/views/classification.vue') },
       { path: 'tag', name: 'tag', component: () => import('/@/views/tag.vue') },
       { path: 'ad', name: 'ad', component: () => import('/@/views/ad.vue') },
       { path: 'notice', name: 'notice', component: () => import('/@/views/notice.vue') },
       { path: 'loginLog', name: 'loginLog', component: () => import('/@/views/login-log.vue') },
       { path: 'opLog', name: 'opLog', component: () => import('/@/views/op-log.vue') },
       { path: 'errorLog', name: 'errorLog', component: () => import('/@/views/error-log.vue') },
-      { path: 'sysInfo', name: 'sysInfo', component: () => import('/@/views/sys-info.vue') },
+      { path: 'sysInfo', name: 'sysInfo2', component: () => import('/@/views/sys-info.vue') },
     ]
   },
 ];
